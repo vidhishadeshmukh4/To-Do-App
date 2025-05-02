4a7c30f45ab316673cc5f8d7d118d76fea89834e
Vidhisha <vidhisha.deshmukh@mitaoe.ac.in>
2025-05-02 18:57:32 +0530

    change in header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -16,7 +16,7 @@ function Header() {
         <div>
             <nav className='header bg-slate-200 flex justify-between items-center'>
                 <div className="logo w-1/4 text-center">
-                    <NavLink to="/">Todo App</NavLink>
+                    <NavLink to="/">TODO Application</NavLink>
                 </div>
                 <div className='flex justify-between'>
                     {
